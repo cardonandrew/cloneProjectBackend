571386b6d6b36226b4bd82d438a74e1fcbd1215b
cardonandrew <cardonandrew0@gmail.com>
2023-10-25 21:10:44 -0600

    test

--- db/client.js
+++ db/client.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
-const connectionString = process.env.DATABASE_URL;
-// || "https://localhost:5432/twitterclone";
+const connectionString =
+  process.env.DATABASE_URL || "https://localhost:5432/twitterclone";
 
 const client = new Pool({
   connectionString,
